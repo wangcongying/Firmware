ea04f373b3ac968787459666af7e37c97244bc51
Lorenz Meier <lm@inf.ethz.ch>
2015-06-27 16:05:38 +0200

    Retire attitude-only EKF due to performance and memory consumption considerations

--- makefiles/config_px4fmu-v2_default.mk
+++ makefiles/config_px4fmu-v2_default.mk
@@ -76,7 +76,8 @@ MODULES 	+= modules/land_detector
 #
 # Estimation modules (EKF/ SO3 / other filters)
 #
-MODULES		+= modules/attitude_estimator_ekf
+# Removed from build due to large static allocations
+#MODULES		+= modules/attitude_estimator_ekf
 MODULES		+= modules/attitude_estimator_q
 MODULES		+= modules/ekf_att_pos_estimator
 MODULES		+= modules/position_estimator_inav
